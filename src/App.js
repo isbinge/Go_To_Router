2b6dd279ab40ed2966e34f25cf6fa23d53469cbb
isbinge <1036749024@qq.com>
2020-03-30 23:00:28 +0800

    2

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Link,
+  useRouteMatch
+} from 'react-router-dom'
+
 import './App.css';
+
+const About = () =>{
+  return <h1>This is a About page</h1>
+}
+
+const Topics = (props) =>{
+  let match = useRouteMatch();
+  console.log(props,'match:',match)
+  return <div>
+    <h1>This is a Topics page</h1>
+  </div>
+}
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <Router>
+      <ul>
+      <li>
+            <Link to="/">Home</Link>
+          </li>
+        <li>
+          <Link to={'/about'}>About</Link>
+        </li>
+        <li>
+          <Link to={'/topics'}>Topics</Link>
+        </li>
+      </ul>
+    <Switch>
+      <Route path={'/about'} component={About}/>
+      <Route path={'/topics'} component={Topics}></Route>
+      <Route path='/'>
+        <h1>This is a home page</h1>
+      </Route>
+    </Switch>
+    </Router>
     </div>
   );
 }
